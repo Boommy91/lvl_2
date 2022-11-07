136cdfce5393c0ad4f0deeb1fa9af7dd712f1425
Boommy91 <goshemidze1991@gmail.com>
2022-11-07 22:28:40 +0200

    testing

--- api/v1/changeItem.php
+++ api/v1/changeItem.php
@@ -1,2 +1,4 @@
 <?php
 $storage = json_decode(file_get_contents('storage.json'), true);
+$storage['items'][$_POST['id']] = ["id" => $_POST['id'], "text" => $_POST['text'], "checked" => $_POST['checked']];
+file_put_contents('storage.json', json_encode($storage));
